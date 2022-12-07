94e61c5e2fe8327df6b23152afe8f4254be186ee
swapnakonda <swaruswapna331@gmail.com>
2022-12-07 10:50:34 +0530

    add details of the person

---
+++ input_basic_data.py
@@ -0,0 +1,15 @@
+# import libraries needed
+
+import qrcode
+
+# data needed
+def data():
+    name = input("enter the name: ")
+    age = input("enter the person age:- ")
+    phonenumber = int(input("enter the person phone number:- "))
+    aadhaarnumber = int(input("enter aadhaar number:- ")
+
+                  
+                    
+# qr code
+qr = qrcode.QRCode(
